cda3c3278bd0c6be00fe48e058bb6891e7e57b8e
Keith Olenchak <kenjin.domini@gmail.com>
2015-10-24 16:14:43 +0000

    added deck generation, dealing, and coerce to ensure a value falls within a range.

--- cards.js
+++ cards.js
@@ -1,10 +1,47 @@
-function Deck() {
+function Deck(maxPlayers, minPlayers = 1, deckSize = 52, lowestCard = 1, cardDuplicates = 0) {
   this.deck = [];
-  
+  this.minPlayers = minPlayers;
+  this.maxPlayers = maxPlayers;
+  this.deckSize = deckSize;
+  this.lowestCard = lowestCard;
+  //Not implemented yet, for special card games with card duplicates
+  this.cardDuplicates = 0;
+  this.max = this.deckSize;
+  this.min = this.lowestCard;
+  while (this.deck.length < 52) {
+    var nextCard = Math.floor(Math.random() *
+    (this.max - this.min + 1) + this.min);
+    if (nextCard == this.min) {
+      this.min++;
+    } else if (nextCard == this.max) {
+      this.max--;
+    }
+    if (this.deck.indexOf(nextCard) == -1) {
+      this.deck.push(nextCard);
+    }
+  }
 }
 
 Deck.prototype = {
     deal: function deal(numberPerPlayer, numberOfPlayers) {
-    
+      numberOfPlayers = this.coerce(numberOfPlayers, this.minPlayers, this.maxPlayers);
+      var hands = {};
+      var player = 1;
+      for (var i = 1; i < numberOfPlayers * numberPerPlayer; i++) {
+        var nextCard = this.deck.shift();
+        hands[player].hand.push(nextCard);
+        if (player++ > numberOfPlayers) {
+          player = 1;
+        }
+      }
+    },
+    coerce: function coerce(value, floor, ceiling) {
+      if (!(value >= floor && value <= ceiling)) {
+        if (value > ceiling) {
+          return ceiling;
+        }
+        return floor;
+      }
+      return value;
     }
 };
